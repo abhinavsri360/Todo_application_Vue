57c51ac3db292218eb01344e4ec26863c6449cfa
abhinavsri360 <abhinav.022000@gmail.com>
2020-06-09 15:06:40 +0530

    deployment changes

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:abhinavsri360/Vue_to-do_application.git master:gh-pages
+git push -f git@github.com:abhinavsri360/Todo_application_Vue.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue_to-do_application/'
+    ? '/Todo_application_Vue/'
     : '/'
 }
